6b97f8507dca4911c164aad673a65fc78ce8e4c7
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-07-24 23:26:42 +0100

    Improve doc formatting

--- src/main/kotlin/io/spine/reflect/StackWalkerStackGetter.kt
+++ src/main/kotlin/io/spine/reflect/StackWalkerStackGetter.kt
@@ -43,7 +43,7 @@ internal class StackWalkerStackGetter : StackGetter {
 
     init {
         // Due to b/241269335, we check in constructor whether this implementation
-        // crashes in runtime, and CallerFinder should catch any Throwable caused.
+        // crashes in runtime, and `CallerFinder` should catch any `Throwable` caused.
         @Suppress("unused")
         val unused = callerOf(StackWalkerStackGetter::class.java, 0)
     }

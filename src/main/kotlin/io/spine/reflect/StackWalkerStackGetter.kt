30b5976ede83d7d70c118db91ecaef111a5b324a
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-07-25 10:58:05 +0100

    Improve `@see` reference text

--- src/main/kotlin/io/spine/reflect/StackWalkerStackGetter.kt
+++ src/main/kotlin/io/spine/reflect/StackWalkerStackGetter.kt
@@ -35,7 +35,7 @@ import kotlin.Long.Companion.MAX_VALUE
  * StackWalker based implementation of the [StackGetter] interface.
  *
  * @see <a href="https://github.com/google/flogger/blob/cb9e836a897d36a78309ee8badf5cad4e6a2d3d8/api/src/main/java/com/google/common/flogger/util/StackWalkerStackGetter.java">
- *     Original Java code of Google Flogger</a>
+ *     Original Java code of Google Flogger</a> for historical reference.
  */
 internal class StackWalkerStackGetter : StackGetter {
 
